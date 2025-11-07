1556a66741bee5be5325cbc77529372cdc90211c
Lucy-hub-hash <lucykamitha39@gmail.com>
2025-11-07 14:19:17 +0300

    Create units.c to display offered units

---
+++ units.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Display the header
+    printf("Units Offered This Semester:\n");
+
+    // Display each unit on a new line
+    printf("1. Introduction to Programming\n");
+    printf("2. Data Structures\n");
+    printf("3. Computer Networks\n");
+    printf("4. Database Systems\n");
+    printf("5. Software Engineering\n");
+
+    return 0;
+}
